5f5e427eb287d9ebd38548610360d92a430854b7
carenswijaya01 <carenskurniawanwijaya@gmail.com>
2024-11-02 12:23:19 +0700

    add nvim config

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.list = false
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+vim.o.swapfile = false -- disable swapfiles because they are fucking garbage
+vim.g.lazyvim_php_lsp = "intelephense"
